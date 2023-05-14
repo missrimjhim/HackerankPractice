7b18a881871918de4a5158769b7ca4565c75278c
missrimjhim <arpitamarch092@gmail.com>
2023-05-14 22:12:34 +0530

    hackerrankpractice-Life,uni,everything

---
+++ src/Life_Universe_Everything.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Life_Universe_Everything {
+    public static void main(String args[]) {
+
+
+    Scanner sc = new Scanner(System.in);
+    int n = 0;
+
+    while(true)
+
+    {
+        n = sc.nextInt();
+        if (n == 42) {
+            break;
+        } else
+            System.out.println(n);
+    }
+
+}
+
+}
